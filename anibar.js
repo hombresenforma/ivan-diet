02a5b678c481cfe62dd8d9eb992d8c7654179f25
hombresenforma <ivan.hombresenforma@gmail.com>
2026-02-02 19:26:05 +0100

    Update anibar.js via Coach App Editor

---
+++ anibar.js
@@ -0,0 +1,19 @@
+export const nutritionData = {
+  plans: [
+    {
+      name: "Plan Estándar",
+      meals: [
+        { 
+          id: "m1", 
+          name: "Desayuno", 
+          options: [
+            { 
+              name: "Opción 1",
+              foods: [{ name: "Avena con Proteína", quantity: "50g", macros: { p: 30, c: 40, f: 5, kcal: 325 } }] 
+            }
+          ] 
+        }
+      ]
+    }
+  ]
+};
